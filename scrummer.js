348ec7e9eacb94af71b991163305ea6900d9414c
Rick Pastoor <rick@blendle.nl>
2015-09-07 22:51:29 +0200

    Tabs to spaces

--- scrummer.js
+++ scrummer.js
@@ -1,37 +1,37 @@
 var debounce = function(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
 
 var containsNodeWithClass = function (nodeList, className) {
-	for (var i = 0; i < nodeList.length; i++) {
-		if (nodeList[i].classList && nodeList[i].classList.contains(className)) {
-			return true;
-		}
-	}
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].classList && nodeList[i].classList.contains(className)) {
+      return true;
+    }
+  }
 }
 
 var listChangeObserver = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
-		if ((mutation.addedNodes.length === 1 && containsNodeWithClass(mutation.addedNodes, 'scrummer-points')) ||
-				(mutation.removedNodes.length === 1 && containsNodeWithClass(mutation.removedNodes, 'scrummer-points'))) {
-			return;
-		}
+    if ((mutation.addedNodes.length === 1 && containsNodeWithClass(mutation.addedNodes, 'scrummer-points')) ||
+        (mutation.removedNodes.length === 1 && containsNodeWithClass(mutation.removedNodes, 'scrummer-points'))) {
+      return;
+    }
 
     // If the list was modified, recalculate
     if (mutation.target.classList.contains('list-cards') ||
         mutation.target.classList.contains('list-header-num-cards') ||
-				mutation.target.classList.contains('js-list-sortable')) {
+        mutation.target.classList.contains('js-list-sortable')) {
       setTimeout(calculateStoryPointsForBoardDebounced);
       return;
     }
@@ -70,8 +70,8 @@ var calculateStoryPointsForCard = function (card) {
 
   if (!originalTitle || cardNameElement.getAttribute('data-mutated') == 1) {
     originalTitle = cardNameElement.lastChild.textContent;
-		cardNameElement.setAttribute('data-mutated', 0);
-		card.setAttribute('data-original-title', originalTitle);
+    cardNameElement.setAttribute('data-mutated', 0);
+    card.setAttribute('data-original-title', originalTitle);
   }
 
   if (!originalTitle) {
@@ -81,7 +81,7 @@ var calculateStoryPointsForCard = function (card) {
   var calculatedPoints = calculateStoryPointsForTitle(originalTitle);
 
   // If the calculated points are not different from what we have
-	// (double == is to compare parsed floats and strings correctly)
+  // (double == is to compare parsed floats and strings correctly)
   if (card.getAttribute('data-calculated-points') == calculatedPoints) {
     return calculatedPoints;
   }
@@ -98,7 +98,7 @@ var calculateStoryPointsForCard = function (card) {
 
     cardNameElement.lastChild.textContent = originalTitle.replace('(' + calculatedPoints + ')', '').trim();
 
-		card.setAttribute('data-calculated-points', calculatedPoints);
+    card.setAttribute('data-calculated-points', calculatedPoints);
   } else if (badgeElement) {
     badgeElement.parentNode.removeChild(badgeElement);
   }
@@ -126,10 +126,10 @@ var calculateStoryPointsForList = function (list) {
 
   var cards = list.querySelectorAll('.list-card:not(.hide)');
   for (var i = 0; i < cards.length; i++) {
-		var cardPoints = calculateStoryPointsForCard(cards[i]);
-		if (cardPoints && cardPoints !== '?') {
-    	listPoints += cardPoints;
-		}
+    var cardPoints = calculateStoryPointsForCard(cards[i]);
+    if (cardPoints && cardPoints !== '?') {
+      listPoints += cardPoints;
+    }
   }
 
   var listHeader = list.querySelector('.js-list-name');
@@ -149,7 +149,7 @@ var calculateStoryPointsForBoard = function () {
     calculateStoryPointsForList(lists[i]);
   }
 
-	listChangeObserver.observe(document.querySelector('.js-list-sortable'), {
+  listChangeObserver.observe(document.querySelector('.js-list-sortable'), {
     childList: true,
     characterData: false,
     attributes: false
@@ -161,39 +161,39 @@ var calculateStoryPointsForBoardDebounced = function () {
 }
 
 var checkForLists = function () {
-	if (document.querySelectorAll('.list').length > 0) {
-		calculateStoryPointsForBoard();
+  if (document.querySelectorAll('.list').length > 0) {
+    calculateStoryPointsForBoard();
 
-		setupWindowListener(function () {
-			if (document.querySelector('.scrummer-picker-container')) {
-				return;
-			}
+    setupWindowListener(function () {
+      if (document.querySelector('.scrummer-picker-container')) {
+        return;
+      }
 
-			var editControls = document.querySelector('.edit .edit-controls');
+      var editControls = document.querySelector('.edit .edit-controls');
 
-			editControls.insertBefore(buildPicker(['X', '?', '0', '1', '3', '5', '8', '13', '20', '40', '100'], function (value, e) {
-				e.stopPropagation();
+      editControls.insertBefore(buildPicker(['X', '?', '0', '1', '3', '5', '8', '13', '20', '40', '100'], function (value, e) {
+        e.stopPropagation();
 
-				var titleField = document.querySelector('.window-title .edit .field');
+        var titleField = document.querySelector('.window-title .edit .field');
 
-				// Remove old points
-				var storypointsForTitle = calculateStoryPointsForTitle(titleField.value);
-				var cleanedTitle = titleField.value.replace('(' + storypointsForTitle + ')', '').trim();
+        // Remove old points
+        var storypointsForTitle = calculateStoryPointsForTitle(titleField.value);
+        var cleanedTitle = titleField.value.replace('(' + storypointsForTitle + ')', '').trim();
 
-				// Prepend new points
-				if (value !== 'X') {
-					titleField.value = '(' + value + ') ' + cleanedTitle;
-				} else {
-					titleField.value = cleanedTitle;
-				}
+        // Prepend new points
+        if (value !== 'X') {
+          titleField.value = '(' + value + ') ' + cleanedTitle;
+        } else {
+          titleField.value = cleanedTitle;
+        }
 
-				// Close and save
-				editControls.querySelector('.js-save-edit').click();
-			}), editControls.firstChild);
-		});
-	} else {
-		setTimeout(checkForLists, 300);
-	}
+        // Close and save
+        editControls.querySelector('.js-save-edit').click();
+      }), editControls.firstChild);
+    });
+  } else {
+    setTimeout(checkForLists, 300);
+  }
 }
 
 // Launch the plugin by checking at a certain interval if any
@@ -204,37 +204,37 @@ checkForLists();
  * The point picker
  */
 var buildPicker = function (values, callback) {
-	var itemsContainer = document.createElement('div');
-	itemsContainer.className = 'scrummer-picker-container';
+  var itemsContainer = document.createElement('div');
+  itemsContainer.className = 'scrummer-picker-container';
 
-	values.forEach(function (value) {
-		var button = document.createElement('a');
-		button.innerText = value;
-		button.addEventListener('click', callback.bind(this, value));
-		button.href = 'javascript:;';
-		button.className = 'scrummer-picker-button';
-		itemsContainer.appendChild(button);
-	});
+  values.forEach(function (value) {
+    var button = document.createElement('a');
+    button.innerText = value;
+    button.addEventListener('click', callback.bind(this, value));
+    button.href = 'javascript:;';
+    button.className = 'scrummer-picker-button';
+    itemsContainer.appendChild(button);
+  });
 
-	return itemsContainer;
+  return itemsContainer;
 }
 
 /**
  * This sets up a listener to see if a detail window is presented
  */
 var setupWindowListener = function (callback) {
-	var windowChangeObserver = new MutationObserver(function (mutations) {
-		mutations.forEach(function (mutation) {
-			if (mutation.target.classList.contains('edit-controls')) {
-				callback();
-			}
-		});
-	});
+  var windowChangeObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.target.classList.contains('edit-controls')) {
+        callback();
+      }
+    });
+  });
 
-	windowChangeObserver.observe(document.querySelector('.window-overlay'), {
-		childList: true,
-		characterData: false,
-		attributes: false,
-		subtree: true
-	});
+  windowChangeObserver.observe(document.querySelector('.window-overlay'), {
+    childList: true,
+    characterData: false,
+    attributes: false,
+    subtree: true
+  });
 }
