c854fac563fa023cc219a186c753598ae69a477c
Rick Pastoor <rick@blendle.nl>
2015-09-18 22:02:47 +0200

    Set up travis and move files to prepare multiplatform

---
+++ tools/make-chromium.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script assumes a linux environment
+
+echo "*** scrummer(Chromium): Creating package"
+echo "*** scrummer(Chromium): Copying files"
+
+DES=./dist/build/scrummer.chromium
+rm -rf $DES
+mkdir -p $DES
+
+cp -R ./src/*                           $DES/
+cp -R ./platform/chromium/img/*         $DES/img/
+cp    ./platform/chromium/manifest.json $DES/
+
+if [ "$1" = all ]; then
+    echo "*** scrummer.chromium: Creating package..."
+    pushd $(dirname $DES/)
+    zip scrummer.chromium.zip -qr $(basename $DES/)/*
+    popd
+fi
+
+echo "*** scrummer(Chromium): Package done."
