76809a8e14fbfc0e45b44cd355ab3606a2dbad01
Rick Pastoor <rick@blendle.nl>
2015-09-18 21:16:06 +0200

    Fixed the not working debounce method
    I implemented a debounce to make sure the plugin was running as smooth as possible, but the debounce method was not actually working.
    
    This commit fixes that.

--- scrummer.js
+++ scrummer.js
@@ -1,14 +1,15 @@
+var debounceTimeout;
+
 var debounce = function(func, wait, immediate) {
-  var timeout;
   return function() {
     var context = this, args = arguments;
     var later = function() {
-      timeout = null;
+      debounceTimeout = null;
       if (!immediate) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+    var callNow = immediate && !debounceTimeout;
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
 };
