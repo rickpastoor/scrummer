03455ae21568a148cd9afa35d9dae3ebd2054e26
Rick Pastoor <rick@blendle.com>
2017-11-14 10:51:56 +0100

    Tried to make it New Firefox Compatible

--- platform/firefox/index.js
+++
@@ -1,9 +0,0 @@
-var data = require("sdk/self").data;
-var pageMod = require("sdk/page-mod");
-
-pageMod.PageMod({
-  include: "*.trello.com",
-  contentScriptFile: data.url("scrummer.js"),
-  contentStyleFile: data.url("scrummer.css"),
-  attachTo: ["top"]
-});
