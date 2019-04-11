42cc5afd37c6e2c87f98add3fe41eb2a283715b9
gabriel andrin <gabriela@theodo.fr>
2019-04-11 13:57:19 +0200

    Change our mutations detection and add setting (+lint)

--- src/settings.js
+++ src/settings.js
@@ -1,12 +1,27 @@
-['showCardNumbers', 'showStoryPoints', 'showPostPoints', 'showColumnTotals', 'showBoardTotals', 'showPicker']
-.forEach((option) => {
-  var checkboxDiv = document.getElementById(option);
+;[
+  'showCardNumbers',
+  'showStoryPoints',
+  'showPostPoints',
+  'showHourPoints',
+  'showColumnTotals',
+  'showBoardTotals',
+  'showPicker'
+].forEach(option => {
+  var checkboxDiv = document.getElementById(option)
 
-  chrome.storage.sync.get(option, (items) => {
-    checkboxDiv.checked = items[option] === undefined ? true : items[option];
-  });
+  chrome.storage.sync.get(option, items => {
+    if (items[option] !== undefined) {
+      checkboxDiv.checked = items[option]
+      return
+    }
+    if ('showHourPoints' === option) {
+      checkboxDiv.checked = false
+      return
+    }
+    checkboxDiv.checked = true
+  })
 
-  checkboxDiv.addEventListener('click', (event) => {
-    chrome.storage.sync.set({ [event.target.id]: event.target.checked });
-  });
-});
+  checkboxDiv.addEventListener('click', event => {
+    chrome.storage.sync.set({ [event.target.id]: event.target.checked })
+  })
+})
